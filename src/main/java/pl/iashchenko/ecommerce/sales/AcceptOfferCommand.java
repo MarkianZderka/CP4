c6a6c32a634017a71f0bdca784da90d6be2a2893
Markiian Zderka <markianzderka@gmail.com>
2025-05-21 13:21:55 +0200

    21 may class

---
+++ src/main/java/pl/iashchenko/ecommerce/sales/AcceptOfferCommand.java
@@ -0,0 +1,9 @@
+package pl.iashchenko.ecommerce.sales;
+
+public class AcceptOfferCommand {
+
+    String firstName;
+    String lastName;
+    String email;
+
+}
---
+++ src/main/java/pl/iashchenko/ecommerce/sales/SalesConfiguration.java
@@ -0,0 +1,13 @@
+package pl.iashchenko.ecommerce.sales;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SalesConfiguration {
+
+    @Bean
+    SalesFacade createMySalesFacade(){
+        return new SalesFacade();
+    }
+}
